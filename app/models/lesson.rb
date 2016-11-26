527f4217a56a4cf8247d2df670cee97e83db12fb
JehuGuillen <jehu.guillen@icloud.com>
2016-11-25 21:04:22 -0600

    Cambios Hechos

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,8 @@
 class Lesson < ApplicationRecord
   has_many :questions
   belongs_to :skill
+
+  def complete_name
+  	"#{self.skill.course.name} #{self.skill.name} #{self.name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-
-
   resources :users
   resources :courses 
   resources :skills 
